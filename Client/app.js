59c485fb842dae6bfcd56ba711c2d8d769246ba4
Dana Kiehl <dana.palm87@gmail.com>
2020-06-02 15:29:07 -0500

    added a jQuery function for the put method to update a movie.

--- Client/app.js
+++ Client/app.js
@@ -26,3 +26,32 @@
     $('#my-form').submit( processForm );
 })(jQuery);
 
+(function($){
+    function updateMovie( e ){
+        var dict = {
+            MovieId: this["movieId"].value,
+        	Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data, textStatus, jQxhr ){
+                $('#response pre').html( data );
+            },
+            error: function( jqXhr, textStatus, errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+
+    $('#update').submit( updateMovie );
+})(jQuery);
+
