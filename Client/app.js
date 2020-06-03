4aeedf085b938216886160f11147a1bdad57bd2e
tim-morozov <tim.morozov@gmail.com>
2020-06-03 15:40:44 -0500

    added function to get a movie by id

--- Client/app.js
+++ Client/app.js
@@ -92,5 +92,21 @@ function updateMovie( e ){
 
 $('#update').submit( updateMovie );
 
+function getById(){
+    var id = $('#movieId').val();
+    var movie = '';
+    $.ajax({
+        url: 'https://localhost:44325/api/movie/' + id,
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        success: function( data, textStatus, jQxhr ){
+            movie = data;
+            console.log(movie);
+           
+        },
+    })
+}
+
 
    
