6829472ddec85ae94e6eed6896f719c1e75dada2
Dana Kiehl <dana.palm87@gmail.com>
2020-06-03 11:40:26 -0500

    removed some spacing, moved the CreateTable method call in two of the function outside of sucess.

--- Client/app.js
+++ Client/app.js
@@ -1,85 +1,88 @@
+function createTable(){
+    $('#movies').html('');
 
-    function createTable(){
-        $('#movies').html('');
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'get',
-            contentType: 'application/json',
-            success: function( data){
-                console.log(data);
-                $.each(data, function(index, value) {
-                    var row = $('<tr><td>' + value.title + '</td><td>' + value.director + '</td><td>' + value.genre + '<td></tr>')
-                    $('#movies').append(row);
-                })
-                
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-
-    }
-
-    $(document).ready(createTable);
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        success: function( data){
+            console.log(data);
+            $.each(data, function(index, value) {
+                var row = $('<tr><td>' + value.title + '</td><td>' + value.director + '</td><td>' + value.genre + '<td></tr>')
+                $('#movies').append(row);
+            })
+            
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
 
 
+}
 
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
-
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-                createTable();
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
-
-        e.preventDefault();
-    }
-
-    $('#my-form').submit( processForm );
+$(document).ready(createTable);
 
 
 
-    function updateMovie( e ){
-        var dict = {
-            MovieId: parseInt(this["movieId"].value),
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
+function processForm( e ){
+    var dict = {
+        Title : this["title"].value,
+        Director: this["director"].value,
+        Genre: this["genre"].value
+    };
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'put',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-                createTable();
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(dict),
+        success: function( data, textStatus, jQxhr ){
+            $('#response pre').html( data );
+            
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+       
+    });
+    createTable();
+    e.preventDefault();
+}
 
-        e.preventDefault();
-    }
+$('#my-form').submit( processForm );
 
-    $('#update').submit( updateMovie );
+
+
+function updateMovie( e ){
+    var dict = {
+        MovieId: parseInt(this["movieId"].value),
+        Title : this["title"].value,
+        Director: this["director"].value,
+        Genre: this["genre"].value
+    };
+
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'put',
+        contentType: 'application/json',
+        data: JSON.stringify(dict),
+        success: function( data, textStatus, jQxhr ){
+            $('#response pre').html( data );
+           
+        },
+        error: function( jqXhr, textStatus, errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+    createTable();
+    e.preventDefault();
+}
+
+$('#update').submit( updateMovie );
+
+
+   
